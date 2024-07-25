f92a009a325661523c9daeb0665e60646ddf2b3f
Draft Sama <draft.sama@gmail.com>
2024-07-25 16:46:17 +0700

    update

--- index.ts
+++ index.ts
@@ -1,16 +1,40 @@
-import server from "bunrest";
-const app = server();
-const router = app.router();
+// import server from "bunrest";
+// const app = server();
+// const router = app.router();
 
 
-// Define a route
-router.get('/check', (req, res) => {
-    res.send('Hello World!');
-});
+// // Define a route
+// router.get('/check', (req, res) => {
+//     res.send('Hello World!');
+// });
 
-app.use("/", router);
+// app.use("/", router);
 
 
-app.listen(34213, () => {
-    console.log('App is listening on port 34213');
-});
+// app.listen(34213, () => {
+//     console.log('App is listening on port 34213');
+// });
+const promise = () =>
+    new Promise<boolean>((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 1);
+    });
+
+Bun.serve({
+    port: 34213,
+    async fetch(request, server) {
+        const pathname = new URL(request.url).pathname;
+        
+      if (pathname === "/works") {
+        return new Response(`Your IP is ${server.requestIP(request)?.address}`);
+      }
+  
+      if (pathname === "/fails") {
+        await promise();
+        return new Response(`Your IP is ${server.requestIP(request)?.address}`);
+      }
+  
+      return new Response("404");
+    },
+  });
